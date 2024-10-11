3c2e19e4251c2fee4365a9adb24d9e9e75e7fe5d
Yixiang Gao <ygao@missouri.edu>
2024-10-11 16:50:36 -0500

    add driver launch and example config

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+from glob import glob
+
+package_name = 'spot_mine_navigation'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob('launch/*launch.[pxy][yma]*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Yixiang Gao',
+    maintainer_email='ygao@missouri.edu',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
