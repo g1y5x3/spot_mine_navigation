a76f539a46b2546bd556c52821d4e132873f46cf
Yixiang <ygao@missouri.edu>
2024-10-22 09:27:17 -0500

    fix config

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'talker = spot_mine_navigation.thermal_image_publisher:main'
+            'thermal_publisher = spot_mine_navigation.thermal_image_publisher:main'
         ],
     },
 )
