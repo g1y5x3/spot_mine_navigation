3a9473a1be7ea316f4994ad72e40d288a372ec17
Yixiang <ygao@missouri.edu>
2024-10-22 10:56:24 -0500

    add parameter reading, will be taken care through the launch file next

--- spot_mine_navigation/thermal_image_publisher.py
+++ spot_mine_navigation/thermal_image_publisher.py
@@ -1,11 +1,25 @@
 import rclpy
 from rclpy.node import Node
+from rclpy.impl import rcutils_logger
 from std_msgs.msg import String
-from spot_wrapper.cam_wrapper import SpotCamCamera, SpotCamWrapper
+
+from spot_wrapper.cam_webrtc_client import WebRTCClient
+from spot_wrapper.wrapper import SpotWrapper
 
 class thermalPublisher(Node):
     def __init__(self):
         super().__init__('thermal_publisher')
+        self.declare_parameter('username', "spot")
+        self.declare_parameter('password', "12345")
+        self.declare_parameter('hostname', "255.255.255.255")
+        self.hostname = self.get_parameter('hostname').value
+        self.username = self.get_parameter('username').value
+        self.password = self.get_parameter('password').value
+
+        print(self.hostname)
+        print(self.username)
+        print(self.password)
+
         self.publisher_ = self.create_publisher(String, 'topic', 10)
         timer_period = 0.5 # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
@@ -20,6 +34,7 @@ class thermalPublisher(Node):
 
 def main(args=None):
     rclpy.init(args=args)
+    logger = rcutils_logger.RcutilsLogger(name=f"thermal_publisher")
     thermal_publisher = thermalPublisher()
     rclpy.spin(thermal_publisher)
 
