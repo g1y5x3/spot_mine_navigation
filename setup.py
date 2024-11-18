e8985b09dcf03752bc1a263a82d3fac9b81e53fe
Yixiang Gao <ygao@missouri.edu>
2024-11-18 09:02:50 -0600

    Revert "start re-organize the repo"
    This reverts commit b131511243d67d304531ef7bf6633789fdf8a0da.

--- setup.py
+++ setup.py
@@ -8,22 +8,23 @@ setup(
     version='0.0.1',
     packages=find_packages(exclude=['test']),
     data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
-        ('share/' + package_name + '/spot_mine_navigation_bringup/launch', glob('launch/*launch.[pxy][yma]*')),
-        # ('share/' + package_name + '/worlds', glob('worlds/*.wbt')),
-        # ('share/' + package_name + '/resource', glob('resource/*')),
+        ('share/' + package_name + '/launch', glob('launch/*launch.[pxy][yma]*')),
+        ('share/' + package_name + '/worlds', glob('worlds/*.wbt')),
+        ('share/' + package_name + '/resource', glob('resource/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Yixiang Gao',
     maintainer_email='ygao@missouri.edu',
-    description='Using spot to automously navigate underground mine, a fully configured gazebo simulation is also included.',
+    description='TODO: Package description',
     license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'thermal_publisher = spot_mine_navigation_application.src.thermal_image_publisher:main'
+            'thermal_publisher = spot_mine_navigation.thermal_image_publisher:main'
         ],
     },
 )
